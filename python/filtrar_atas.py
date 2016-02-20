812fb25ed5b78349f63b7649c016b5885a733b28
Lucas Murtinho <lucas.murtinho@gmail.com>
2016-02-20 20:19:44 -0200

    lista de vereadores (a limpar)

--- python/filtrar_atas.py
+++ python/filtrar_atas.py
@@ -5,6 +5,7 @@ Created on Fri Feb 19 22:33:21 2016
 @author: lucas_000
 """
 
+import itertools
 import os
 import re
 from txt_to_html import arquivos
@@ -32,10 +33,37 @@ def filtro_string(atas, regex):
     return {nome: atas[nome] for nome in atas
             if re.findall(regex, atas[nome])}
 
-arquivos_txt = arquivos(diretorio, 'txt')
-#atas_votos = filtro_string(diretorio, arquivos_txt, r'vota[ram|ção]')
-atas_2016 = filtro_inicio_arquivo(diretorio, arquivos_txt, '2015')
-votos_2016 = filtro_string(atas_2016, r'vota[ram|ção]')
+def pegar_votantes(votos):
+    sessoes_com_voto = [re.findall(r'(?:votou|votaram) (.*?) o[s]? (?:Senhor|Senhores) (?:Vereador|Vereadores) (.*?) \((.*?)\)', 
+                        votos_2015[ata]) 
+               for ata in sorted(list(votos))]
+    votantes = []
+    for sessao in sessoes_com_voto:
+        for voto in sessao:
+            if ',' in voto[1]:
+                votantes.append(re.findall(r'(\b.*?),', voto[1]) + re.findall(' e (.*)$', voto[1]))
+            elif ' e ' in voto[1]:
+                votantes.append(voto[1].split(' e '))
+            else:
+                votantes.append(voto[1])
+    return votantes
 
-# próximo passo: filtrar por ano (4 primeiros dígitos do nome do arquivo)
+arquivos_txt = arquivos(diretorio, 'txt')
+atas_2015 = filtro_inicio_arquivo(diretorio, arquivos_txt, '2015')
+votos_2015 = filtro_string(atas_2015, r'votaram|votação')
+
+#voto_regex = re.compile(r"""(?:votou|votaram) 
+#                          (.*?) # matéria do voto
+#                          o[s]? #
+#                          (?:Senhor|Senhores) #
+#                          (?:Vereador|Vereadores) #
+#                          (.*?) # vereadores votantes 
+#                          \(#
+#                          (.*?)# número de votantes
+#                          \)""", re.X)
+#votaram2 = [re.findall(voto_regex, votos_2015[ata])
+#           for ata in sorted(list(votos_2015))]
+
+votantes_por_voto = pegar_votantes(votos_2015)
+vereadores = set(itertools.chain(*votantes_por_voto))
 
