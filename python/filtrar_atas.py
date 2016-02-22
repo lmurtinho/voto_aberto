a35966d49d0ae0e51e09a4fc0f11dd4531c0da97
Lucas Murtinho <lucas.murtinho@gmail.com>
2016-02-21 22:37:42 -0300

    lista de vereadores 2015 pronta

--- python/filtrar_atas.py
+++ python/filtrar_atas.py
@@ -29,45 +29,60 @@ def filtro_inicio_arquivo(diretorio, arquivos, inicio):
     os.chdir(diretorio_atual)
     return filtrados
 
-def filtro_string(atas, regex):
-    return {nome: atas[nome] for nome in atas
-            if re.findall(regex, atas[nome])}
+def filtro_regex(dicionario, regex):
+    """
+    Retorna todos os elementos no dicionário
+    que contenham a regex.
+    """
+    return {nome: dicionario[nome] 
+            for nome in dicionario
+            if re.findall(regex, dicionario[nome])}
 
-def pegar_votantes(votos):
-    sessoes_com_voto = [re.findall(r'(?:votou|votaram) (.*?) o[s]? (?:senhor|senhores) (?:vereador|vereadores) ([^;\.\(\d]+[^\W\d_])', 
-                        votos_2015[ata])
-                        for ata in sorted(list(votos))]
+def pegar_votantes(atas):
+    """
+    O argumento é um dicionário de strings em que cada string é uma ata.
+    O retorno é uma lista com os votantes para cada voto registrado
+    em cada ata.
+    """
+    # retira todos os votos em todas as atas
+    todos_os_votos = [{'data': ata,
+                       'votos': re.findall(r'(?:votou|votaram) (.*?) [oa][s]? (?:senhor[a]?|senhor[ea]s) (?:vereador[a]?|vereador[ea]s) ([^;\.\(\d]+[^\W\d_])', 
+                      atas[ata])}
+                      for ata in sorted(list(atas))]
     votantes = []
-    for sessao in sessoes_com_voto:
-        for voto in sessao:
-            if ',' in voto[1]:
-                votantes.append(re.findall(r'(\b.*?),', voto[1]) + re.findall(' e (.*)$', voto[1]))
-            elif ' e ' in voto[1]:
-                votantes.append(voto[1].split(' e '))
+    for sessao in todos_os_votos:
+        for votacao in sessao['votos']:
+            # mais de dois votantes: último separado por ' e ', 
+            # demais por vírgula
+            voto = {'data': sessao['data']}            
+            if ',' in votacao[1]:
+                voto['voto'] = re.findall(r'(\b.*?),', votacao[1]) + re.findall(' e (.*)$', votacao[1])
+            # dois votantes separados por ' e '
+            elif ' e ' in votacao[1]:
+                voto['voto'] = votacao[1].split(' e ')
+            # um votante
             else:
-                votantes.append(voto[1])
+                voto['voto'] = votacao[1]
+            votantes.append(voto)
     return votantes
 
 arquivos_txt = arquivos(diretorio, 'txt')
 atas_2015 = filtro_inicio_arquivo(diretorio, arquivos_txt, '2015')
-votos_2015 = filtro_string(atas_2015, r'votaram|votação')
-
-#voto_regex = re.compile(r"""(?:votou|votaram) 
-#                          (.*?) # matéria do voto
-#                          o[s]? #
-#                          (?:Senhor|Senhores) #
-#                          (?:Vereador|Vereadores) #
-#                          (.*?) # vereadores votantes 
-#                          \(#
-#                          (.*?)# número de votantes
-#                          \)""", re.X)
-#votaram2 = [re.findall(voto_regex, votos_2015[ata])
-#           for ata in sorted(list(votos_2015))]
-
-# regex antiga: r'(?:votou|votaram) (.*?) o[s]? (?:Senhor|Senhores) (?:Vereador|Vereadores) (.*?) \((.*?)\)'
-
+votos_2015 = filtro_regex(atas_2015, r'votaram|votação')
 votantes_por_voto = pegar_votantes(votos_2015)
-vereadores = set(itertools.chain(*votantes_por_voto))
-vereadores = {vereador.strip() 
-              for vereador in vereadores
-              if len(vereador)>2}
+
+vereadores = []
+for voto in votantes_por_voto:
+    if type(voto['voto']) == str:
+        vereadores.append(voto['voto'])
+    else:
+        vereadores.extend(list(voto['voto']))
+vereadores = set(vereadores)
+
+#vereadores = set([vereador for vereador in 
+#              itertools.chain(*[voto['voto'] for voto in votantes_por_voto])
+#              if len(vereador)>2])
+erros = [{'data': voto['data'], 
+          'erros': [string for string in voto['voto'] if len(string)<3]} 
+          for voto in votantes_por_voto]
+erros = [erro for erro in erros if erro['erros']]
